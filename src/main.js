892ff3243e1aceea2ecb3848193ee1b9e0d9eca2
lcw1314666666 <1820566696@qq.com>
2020-10-30 17:40:00 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import './assets/reset.css'
 import './assets/border.css'
 import './assets/style/iconfont.css'
 
+import 'swiper/dist/css/swiper.css'
+
+
+// Vue.use(VueAwesomeSwiper)
+
 fastClick.attach(document.body)
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAwesomeSwiper).mount('#app')
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="home">
         <HomeHeader></HomeHeader>
+        <HomeSwiper></HomeSwiper>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/header.vue'
+import HomeSwiper from './components/swiper.vue'
 export default {
     name: 'Home',
     components: {
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 }
 </script>
 
 <style scoped>
+
 </style>
