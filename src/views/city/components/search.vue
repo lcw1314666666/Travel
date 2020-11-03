bd877e86b4d729cf14c1cc5a9b2de63c5e32e64e
lcw1314666666 <1820566696@qq.com>
2020-11-03 20:37:17 +0800

    add city search

---
+++ src/views/city/components/search.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="请输入城市名称或拼音">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style scoped>
+    .search{
+        position: relative;
+        width: 100%;
+        height: .72rem;
+        padding: 0 .1rem;
+        background: #00bcd4
+    }
+    .search .search-input{
+        position: absolute;
+        box-sizing: border-box;
+        height: .62rem;
+        line-height: .62rem;
+        padding: 0 .1rem;
+        color: #666;
+        text-align: center;
+        border-radius: 0.06rem;
+        width: 94%;
+        outline: none;
+    }
+</style>
