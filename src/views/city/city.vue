42fd3f135c0e8f45f8e181cb1f3ff45cebb55852
lcw1314666666 <1820566696@qq.com>
2020-11-07 21:59:31 +0800

    add city-search-logic

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="city">
         <CityHeader></CityHeader>
-        <CitySearch></CitySearch>
+        <CitySearch :cities="cities"></CitySearch>
         <CityList 
             :hotCities="hotCities" 
             :cities="cities"
--- src/views/city/components/search.vue
+++ src/views/city/components/search.vue
@@ -1,13 +1,67 @@
-<template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="请输入城市名称或拼音">
+<template> 
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="请输入城市名称或拼音">
+        </div>
+        <div class="search-content"
+             ref="search" 
+             v-show="keyword">
+            <ul>
+                <li class="search-item boredr-bottom" 
+                    v-for="item in list" 
+                    :key="item.id"
+                >{{ item.name }}</li>
+                <li class="search-item boredr-bottom" 
+                    v-show="!list.length"    
+                >未找到相应的城市</li>
+            </ul>
+        </div>
     </div>
+    
 </template>
 
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    data () {
+        return {
+            keyword: '',
+            list: [],
+            timer: null
+        }
+    },
+    props: {
+        cities: Object
+    },
+    watch: {
+        keyword () {
+            const list = []
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            this.timer = setTimeout(() => {
+                if (!this.keyword) {
+                    this.list = []
+                    return 
+                }
+                for (let i in this.cities) {
+                    this.cities[i].forEach((item) => {
+                        // console.log(item)
+                        if (item.spell.indexOf(this.keyword) > -1 || item.name.indexOf(this.keyword) > -1) {
+                            list.push(item)
+                        }
+                    })
+                }
+                this.list = list
+                console.log(list)
+            }, 100)
+        }
+    },
+    mounted () {
+        this.scroll = new BScroll(this.$refs.search)
+    }
 }
 </script>
 
@@ -29,5 +83,22 @@ export default {
         border-radius: 0.06rem;
         width: 97%;
         outline: none;
+    }
+    .search-content{
+        overflow: hidden;
+        position: absolute;
+        top: 1.56rem;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: #fff;
+        z-index: 1;
+    }
+    .search-content .search-item{
+        line-height: 0.6rem;
+        font-size: 0.3rem;
+        color: #666;
+        text-align: center;
+        border-bottom: 0.01rem solid #ccc;
     }
 </style>
